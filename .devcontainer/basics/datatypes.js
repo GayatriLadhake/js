0cbc8490e8320848fa9d46540c24e245f7a25994
Gayatri Ladhake <gayatriladhake20@gmail.com>
2025-07-11 05:44:40 +0000

    variables and datatypes

---
+++ .devcontainer/basics/datatypes.js
@@ -0,0 +1,16 @@
+let age = 21;
+let name = "gayatri";
+let isCorrect = false;
+ console.log(typeof name);
+
+ /* primitive datatypes
+  number => 2 to power 53
+  bigint
+  string=>""
+  null => standalone value
+  undefined =>
+ symbol =>unique
+
+  non primitive datatypes
+  object
+  */
