c48e5223532d516a0d948f9c9e3da2984257af57
Gayatri Ladhake <gayatriladhake20@gmail.com>
2025-07-11 06:28:01 +0000

    conversion of datatypes

---
+++ .devcontainer/basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+
+// 1 => true , 0 => false
+//"" => false
+//"Gayatri" => true
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1 , false => 0
+
+//number to string example
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+//number to boolean
+let number = 10;
+let numberBoolean = Boolean(number);
+console.log(numberBoolean);
+console.log(typeof numberBoolean);
