6f96a9cfb2b9c60bb97499b0baba68ef7001dc19
Gayatri Ladhake <gayatriladhake20@gmail.com>
2025-07-11 06:56:26 +0000

    comparison operation

---
+++ .devcontainer/basics/04_comparison.js
@@ -0,0 +1,17 @@
+//comparison output gives boolean values
+
+
+// console.log(2>1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+
+// when performing comparison datatypes should be same if not it gives some error in result.
+/*console.log("2" > 1);
+console.log("02" > 1);
+*/
+
+//in js equality check and comparison operators works diffrently comparison operator convert values to no and then perform comparison operation
+ /*console.log(null > 0);
+ console.log(null == 0);
+ console.log(null >= 0);
+ */
